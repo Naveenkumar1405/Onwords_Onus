4989958ded0812e9ddb5f40c654c7fd6406523c9
Naveenkumar1405 <121798977+Naveenkumar1405@users.noreply.github.com>
2023-08-17 19:15:36 +0530

    NAVIN THURS 18/08 19:05

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 import requests
 
-fast_api_server_ip = "192.168.1.16:8000"
+fast_api_server_ip = "192.168.1.30:8000"
 
 from datetime import datetime
 
@@ -25,7 +25,6 @@ def get_client_data_using_phonenumber(client_number):
     try:
         client_data = requests.get(f"http://{fast_api_server_ip}/client/{client_number}").json()
     except requests.exceptions.RequestException as e:
-        print(e)
         return e
 
     return client_data
@@ -38,24 +37,21 @@ def convert_pr_uid_to_name(uid):
     for staff in staff_data:
         if uid == staff:
             staff_name = staff_data[staff]['name']
-            print(staff_name)
             return staff_name
+    return None
+
 
 
 # convert_pr_uid_to_name("vymWjMhjOVPuNjoxBlj3HA3nJxC2")
 def convert_to_timestamp(date, time):
     dt_string = date + " " + time
     dt_object = datetime.strptime(dt_string, '%Y-%m-%d %I:%M %p')
-
     timestamp = dt_object.timestamp()
     return timestamp
 
 
 def convert_datetime(date_and_time):
-    print("converting")
     dt_obj = datetime.strptime(date_and_time, '%Y-%m-%dT%H:%M')
-    print(dt_obj)
     date_str = dt_obj.strftime('%d-%m-%Y')
     time_str = dt_obj.strftime('%I:%M%p')
-    print(date_str, time_str)
     return date_str, time_str
